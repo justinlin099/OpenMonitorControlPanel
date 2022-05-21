4c7e87edf5d985e26c473e0f509e42a40ca017e9
Justin Lin <61717681+justinlin099@users.noreply.github.com>
2022-05-22 02:20:02 +0800

    Input Select Complete Alpha 0.1

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ from tkinter import *
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import os
+from functools import partial
 
 #套用樣式
 controlPanel = ttk.Window(themename="cyborg")
@@ -14,11 +15,15 @@ controlPanel.resizable(False,False)
 
 
 #設定視窗參數
-controlPanel.title('OpenMonitorControlPanel')
+controlPanel.title('OpenMonitorControlPanel Alpha 0.1')
 controlPanel.attributes("-alpha", 0.8)
 controlPanel.attributes("-topmost", 1)
 controlPanel.geometry('400x600-0-35')
 
+#導入圖片
+VGAImg = PhotoImage(file = r"VGA.png")
+HDMIImg = PhotoImage(file = r"HDMI.png")
+DPImg = PhotoImage(file = r"DP.png")
 
 #System Tray Code
 
@@ -33,11 +38,20 @@ def show_window(icon, item):
 def withdraw_window():  
     controlPanel.withdraw()
     image = PIL.Image.open("image.ico")
-    menu = (item('離開', quit_window), item('顯示', show_window ,default=True))
+    menu = (item('退出', quit_window), item('顯示', show_window ,default=True))
     icon = pystray.Icon("name", image, "Open Monitor Control Panel", menu)
     icon.run()
 
 controlPanel.protocol("WM_DELETE_WINDOW", withdraw_window)
+
+#切換輸入
+
+def selectInput(monitorString , inputValue):
+    os.system("ControlMyMonitor.exe /SetValue " + monitorString + " 60 "+inputValue)
+    print("ControlMyMonitor.exe /SetValue " + monitorString + " 60 "+inputValue)
+
+
+
 
 #掃描螢幕
 
@@ -46,23 +60,54 @@ def scanMonitor():
     file = open('smonitors.txt', 'r',encoding="utf-16 le")
 
     MonitorList = file.readlines()
+    MonitorList[0]=MonitorList[0][1:]
+    
     messagebox.showinfo("掃描完成" , "找到 " + str(len(MonitorList)//6) + " 個螢幕" )
     file.close()
     Monitors=[]
+    OptionList=[]
+    lf=[]
+    inputButton=[]
+    inputOptions=[]
     for i in range(len(MonitorList)//6):
-        os.system("ControlMyMonitor.exe /scomma monitor"+ str(i) +".txt "+MonitorList[0+6*i][22:])
+        os.system("ControlMyMonitor.exe /scomma monitor"+ str(i) +".txt "+MonitorList[0+6*i][21:])
+        print("ControlMyMonitor.exe /scomma monitor"+ str(i) +".txt "+MonitorList[0+6*i][21:])
         file = open('monitor'+str(i)+'.txt', 'r',encoding="utf-8")
-
         Monitors.append(file.readlines())
         file.close()
-        print(Monitors[i][10])
+        OptionList.append({})
+        for j in range(len(Monitors[i])):
+            OptionList[i][Monitors[i][j][:2]]=Monitors[i][j].split(",",5)
+        
+        print(OptionList[i]["60"][5])
+
+        #生成螢幕控制區塊
+        lf.append(ttk.LabelFrame(controlPanel, text=MonitorList[1+6*i][15:-2]))
+        lf[i].pack(fill='x',padx=10,pady=5)
+        inputButton.append([])
+        
+        inputOptions.append(OptionList[i]["60"][5][1:-2].split(','))
+        for j in range(len(inputOptions[i])):
+            if(int(inputOptions[i][j])<15):
+                action_with_arg = partial(selectInput, MonitorList[0+6*i][21:-1], inputOptions[i][j])
+                inputButton[i].append(ttk.Button(lf[i], text="VGA", image = VGAImg, compound=TOP, bootstyle="secondary",command=action_with_arg))
+                
+            elif(int(inputOptions[i][j])<17):
+                action_with_arg = partial(selectInput, MonitorList[0+6*i][21:-1], inputOptions[i][j])
+                inputButton[i].append( ttk.Button(lf[i], text="DP", image = DPImg, compound=TOP,bootstyle="secondary",command=action_with_arg))
+                
+            else:
+                action_with_arg = partial(selectInput, MonitorList[0+6*i][21:-1], inputOptions[i][j])
+                inputButton[i].append(ttk.Button(lf[i], text="HDMI", image = HDMIImg, compound=TOP,bootstyle="secondary",command=action_with_arg))
+                
+            inputButton[i][j].grid(row=0, column=j, padx=5, pady=5)
+    
     
     
 
 #Setup
 refreshButton = ttk.Button(controlPanel, text="更新螢幕清單", bootstyle="default",command=scanMonitor)
-refreshButton.place(x=300,y=5)
-
+refreshButton.pack(anchor=tk.NE,padx=10,pady=10)
 
 
 
